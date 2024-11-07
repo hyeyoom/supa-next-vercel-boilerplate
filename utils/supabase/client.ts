635262a26fd0cceafa95e138b215b2b71d75dc8f
hyeyoom <neoul_chw@icloud.com>
2024-11-08 00:41:38 +0900

    feat: apply supabase database

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,8 @@
 import {createBrowserClient} from '@supabase/ssr'
+import {Database} from "@/utils/types/supabase";
 
 export function createSupabaseClientForBrowser() {
-    return createBrowserClient(
+    return createBrowserClient<Database>(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     )
